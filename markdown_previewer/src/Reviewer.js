38471cf264eb3b3705d721626e5f1149971f3bca
Vladimir Samsonov <arti_personal@protonmail.com>
2023-04-18 16:53:26 -0600

    changed2marked

--- markdown_previewer/src/Reviewer.js
+++ markdown_previewer/src/Reviewer.js
@@ -1,26 +1,12 @@
 import { useState, useEffect } from 'react';
 import parse from 'html-react-parser';
 import { marked } from 'marked';
+import hljs from 'highlight.js/lib/core';
+import javascript from 'highlight.js/lib/languages/javascript';
+hljs.registerLanguage('javascript', javascript);
 
 function Reviewer(props) {
 
-  const changeText = (str) => {
-    return str.replaceAll(/<code>(?<code>[\w\s\W]*?)<\/code>/g, (match, code) => {
-      return code.replaceAll(/(?<!\'|\")(?<func>function|if|else|return)(?!\'|\")/g, "<span className='blue'>$<func></span>")
-        .replaceAll(/(function<\/span> )(?<name>\w*)( ?\((?!\'|\"))/g, "$1<span className='red'>$<name></span>$3")
-        .replaceAll(/(?<comm>\/\/[\w\s\W]*?)\n/g, "<span className='green'>$<comm><br/></span>")
-        .replaceAll(/(?<comma>&amp;&amp;|\|\||==)/g, "<span className='gold'>$<comma></span>");
-    });
-  }
-
-  let [resultText, setResultText] = useState("");
-  useEffect(() => {
-    const html = marked.parse(props.input);
-    //setResultText(changeText(props.input));
-    setResultText(changeText(html));
-  })
-
-  //<div id="reviwer-container">{parse(resultText)}</div>
   return (
     <div className="container" id="reviewer-container">
       <div className="header-container">
@@ -32,7 +18,7 @@ function Reviewer(props) {
           <i className="fas fa-expand" id="reviewer-button"></i>
         </div>
       </div>
-      <div id="preview">{(parse(resultText))}</div>
+      <div id="preview">{parse(marked.parse(props.input, { breaks: true, langPrefix: "hljs language-javascript" }, hljs.initHighlightingOnLoad()))}</div>
     </div>
   );
 }
