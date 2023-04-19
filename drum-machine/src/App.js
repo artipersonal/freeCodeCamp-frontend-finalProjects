904635862020509a7ec700bc3c377c64ab17f4ba
Vladimir Samsonov <arti_personal@protonmail.com>
2023-04-19 15:51:44 -0600

    full_tests

--- drum-machine/src/App.js
+++ drum-machine/src/App.js
@@ -20,6 +20,18 @@ class App extends React.Component {
     "Closed-HH": new Audio("https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3")
   };
 
+  audio_links = {
+    "Heater-1": "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3",
+    "Heater-2": "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3",
+    "Heater-3": "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3",
+    "Heater-4": "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3",
+    "Clap": "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3",
+    "Open-HH": "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3",
+    "Kick-and-Hat": "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3",
+    "Kick": "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3",
+    "Closed-HH": "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3"
+  };
+
 
 
   constructor(props) {
@@ -31,18 +43,46 @@ class App extends React.Component {
     };
   }
 
+  getKeyByValue(object, value) {
+    return Object.keys(object).find(key => object[key] === value);
+  };
+
   handleChange = (event) => {
-    this.setState({ lastPlayed: event.target.value });
+
+    //this.handleKeyDown({ code: this.getKeyByValue(this.doc, event.target.value) });
+    //this.handleKeyUp({ code: this.getKeyByValue(this.doc, event.target.value) });
+
+    //this.audio[this.doc[event.code]].volume = this.state.volume;
+
+    let btn = document.getElementById(event.target.value);
+    let aud = btn.firstElementChild;
+    aud.volume = this.state.volume;
+    if (this.state.power) {
+      this.setState({ lastPlayed: event.target.value });
+      btn.click();
+      btn.classList.add('active-on');
+      aud.play();
+    }
+    else {
+      btn.classList.add('active-off');
+    }
+    setTimeout(() => {
+      btn.classList.remove('active-on');
+      btn.classList.remove('active-off');
+    }, 50);
+
   };
 
   handleKeyDown = (event) => {
     if (this.doc.hasOwnProperty(event.code)) {
-      this.audio[this.doc[event.code]].volume = this.state.volume;
+      //this.audio[this.doc[event.code]].volume = this.state.volume;
+      let aud = document.getElementById(event.code.slice(3, 5));
+      aud.volume = this.state.volume;
       let btn = document.getElementById(this.doc[event.code]);
       if (this.state.power) {
         btn.click();
         btn.classList.add('active-on');
-        this.audio[this.doc[event.code]].play();
+        aud.play();
       }
       else {
         btn.classList.add('active-off');
@@ -84,15 +124,33 @@ class App extends React.Component {
       <div id="drum-machine">
         <div id="display">
           <div id="buttons">
-            <button className="drum-pad" id="Heater-1" value="Heater-1" onClick={this.handleChange}>Q</button>
-            <button className="drum-pad" id="Heater-2" value="Heater-2" onClick={this.handleChange}>W</button>
-            <button className="drum-pad" id="Heater-3" value="Heater-3" onClick={this.handleChange}>E</button>
-            <button className="drum-pad" id="Heater-4" value="Heater-4" onClick={this.handleChange}>A</button>
-            <button className="drum-pad" id="Clap" value="Clap" onClick={this.handleChange}>S</button>
-            <button className="drum-pad" id="Open-HH" value="Open-HH" onClick={this.handleChange}>D</button>
-            <button className="drum-pad" id="Kick-and-Hat" value="Kick-and-Hat" onClick={this.handleChange}>Z</button>
-            <button className="drum-pad" id="Kick" value="Kick" onClick={this.handleChange}>X</button>
-            <button className="drum-pad" id="Closed-HH" value="Closed-HH" onClick={this.handleChange}>C</button>
+            <button className="drum-pad" id="Heater-1" value="Heater-1" onClick={this.handleChange}>Q
+              <audio controls hidden className="clip" id="Q" src={this.audio_links[this.doc["KeyQ"]]}></audio>
+            </button>
+            <button className="drum-pad" id="Heater-2" value="Heater-2" onClick={this.handleChange}>W
+              <audio controls hidden className="clip" id="W" src={this.audio_links[this.doc["KeyW"]]}></audio>
+            </button>
+            <button className="drum-pad" id="Heater-3" value="Heater-3" onClick={this.handleChange}>E
+              <audio controls hidden className="clip" id="E" src={this.audio_links[this.doc["KeyE"]]}></audio>
+            </button>
+            <button className="drum-pad" id="Heater-4" value="Heater-4" onClick={this.handleChange}>A
+              <audio controls hidden className="clip" id="A" src={this.audio_links[this.doc["KeyA"]]}></audio>
+            </button>
+            <button className="drum-pad" id="Clap" value="Clap" onClick={this.handleChange}>S
+              <audio controls hidden className="clip" id="S" src={this.audio_links[this.doc["KeyS"]]}></audio>
+            </button>
+            <button className="drum-pad" id="Open-HH" value="Open-HH" onClick={this.handleChange}>D
+              <audio controls hidden className="clip" id="D" src={this.audio_links[this.doc["KeyD"]]}></audio>
+            </button>
+            <button className="drum-pad" id="Kick-and-Hat" value="Kick-and-Hat" onClick={this.handleChange}>Z
+              <audio controls hidden className="clip" id="Z" src={this.audio_links[this.doc["KeyZ"]]}></audio>
+            </button>
+            <button className="drum-pad" id="Kick" value="Kick" onClick={this.handleChange}>X
+              <audio controls hidden className="clip" id="X" src={this.audio_links[this.doc["KeyX"]]}></audio>
+            </button>
+            <button className="drum-pad" id="Closed-HH" value="Closed-HH" onClick={this.handleChange}>C
+              <audio controls hidden className="clip" id="C" src={this.audio_links[this.doc["KeyC"]]}></audio>
+            </button>
           </div>
           <div id="tuning">
             <div id="logo">
