67d38fe64fa5ffe643bd7f7cbcd285628f004e2d
Vladimir Samsonov <arti_personal@protonmail.com>
2023-04-20 23:36:06 -0600

    timer-done-with-bugs

---
+++ 25_5app/src/App.js
@@ -0,0 +1,147 @@
+import './App.css';
+import { useState, useRef, useEffect } from 'react';
+
+function App() {
+
+  const [timeNum, setTimeNum] = useState({ time: 1500, status: false, break: false });
+  const [breakLength, setBreakLength] = useState(5);
+  const [sessionLength, setSessionLength] = useState(25);
+
+  const myTimer = useRef(null);
+  let myTimeout = useRef(null);
+
+  useEffect(() => {
+    myTimeout.current = null;
+  }, [timeNum.status]);
+
+
+  // NEED TO CHANGE STATUS ONCLICK AND THEN USE useEFFECT TO LAUNCH A TIMER
+
+
+  useEffect(() => {
+    let check = document.getElementById("time-left");
+    if (!check.classList.contains("red") && (timeNum.time <= 59)) {
+      check.classList.add("red");
+    }
+    else if (timeNum.time > 59) {
+      check.classList.remove("red");
+    }
+    if (timeNum.time == 0) {
+      document.getElementById("beep").play();
+      setTimeNum((prevState) => {
+        if (prevState.break) {
+          return { time: sessionLength, status: true, break: false }
+        }
+        else {
+          return { time: breakLength, status: true, break: true }
+        }
+      })
+    }
+  }, [timeNum.time]);
+
+  const laucnhTimer = () => {
+
+    // if there is no timer - this mean we start our action or the previous action was completed
+    // EQUALS - we may action further
+    if (!myTimeout.current) {
+      myTimeout.current = setTimeout(() => {
+        if (!timeNum.status) {
+          myTimer.current = setInterval(() => {
+            setTimeNum((prevState) => ({ time: prevState.time - 1, status: true, break: prevState.break }));
+
+          }, 1000);
+        }
+      }, 100);
+
+    }
+  }
+
+  const handleReset = () => {
+    clearInterval(myTimer.current);
+    setTimeNum({ time: 1500, status: false, break: false });
+    setBreakLength(5);
+    setSessionLength(25);
+  };
+
+  const handleStop = () => {
+    clearInterval(myTimer.current);
+    setTimeNum((prevState) => ({ time: prevState.time, status: false, break: prevState.break }));
+  }
+
+  return (
+    <div id="App">
+      <div id="container">
+        <div id="header"><h1>25 + 5 Clock</h1></div>
+        <div id="labels">
+          <div id="break-label">
+            <h2>Break Length</h2>
+            <div id="break-container">
+              <button id="break-decrement" onClick={() => {
+                if (breakLength > 1) { setBreakLength((s) => (s - 1)) }
+              }
+              }><i className="fa-solid fa-arrow-down"></i></button>
+              <span id="break-length">{breakLength}</span>
+              <button id="break-increment" onClick={() => {
+                if (breakLength < 60) {
+                  setBreakLength((s) => (s + 1));
+                }
+              }
+              }><i className="fas fa-arrow-up"></i></button>
+            </div>
+          </div>
+          <div id="session-label">
+            <h2>Session Length</h2>
+            <div id="session-container">
+              <button id="session-decrement" onClick={() => {
+                if (sessionLength > 1) {
+                  setSessionLength((state) => {
+                    clearInterval(myTimer.current);
+                    setTimeNum({ time: (state - 1) * 60, status: false, break: false });
+                    return (state - 1)
+                  },)
+                }
+              }
+              }><i className="fa-solid fa-arrow-down"></i></button>
+              <span id="session-length">{sessionLength}</span>
+              <button id="session-increment" onClick={() => {
+                if (sessionLength < 60) {
+                  setSessionLength((state) => {
+                    clearInterval(myTimer.current);
+                    setTimeNum({ time: (state + 1) * 60, status: false, break: false });
+                    return (state + 1)
+                  },)
+                }
+              }
+
+              }><i className="fas fa-arrow-up"></i></button>
+            </div>
+          </div>
+        </div>
+        <div id="timer">
+          <div id="timer-label">{(timeNum.break) ? "Break" : "Session"}</div>
+          <div id="time-left">{`${Math.floor(timeNum.time / 60) >= 10
+            ? Math.floor(timeNum.time / 60)
+            : '0' + Math.floor(timeNum.time / 60).toString()}:${(timeNum.time - Math.floor(timeNum.time / 60) * 60) >= 10
+              ? (timeNum.time - Math.floor(timeNum.time / 60) * 60)
+              : '0' + (timeNum.time - Math.floor(timeNum.time / 60) * 60).toString()}`}</div>
+          <audio hidden controls id="beep" src="https://orangefreesounds.com/wp-content/uploads/2022/12/Clock-gong-sound.mp3"></audio>
+        </div>
+        <div id="controls">
+          <button id="start_stop" onClick={(timeNum.status) ? handleStop : laucnhTimer}>
+            <i className="fa-solid fa-play"></i>
+            <i className="fa-solid fa-pause"></i>
+          </button>
+          <button id="reset" onClick={handleReset}>
+            <i className="fa fa-refresh" aria-hidden="true"></i>
+          </button>
+        </div>
+        <div id="footer">
+          <h3>Designed and Coded by</h3>
+          <h3>Vladimir Samsonov</h3>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
